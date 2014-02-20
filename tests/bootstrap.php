079e63ae1649b38979605f0a40411c4dd786e18d
pp <pp@move-elevator.de>
2014-02-20 15:07:18 +0100

    features, add unit tests for 1d and 2d barcode processing

---
+++ tests/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This is bootstrap for phpUnit unit tests,
+ * use README.md for more details
+ *
+ * @author Gediminas Morkevicius <gediminas.morkevicius@gmail.com>
+ * @author Christoph Krämer <cevou@gmx.de>
+ * @link http://www.gediminasm.org
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+define('TESTS_PATH', __DIR__);
+define('TESTS_TEMP_DIR', __DIR__ . '/temp');
+
+if (!class_exists('PHPUnit_Framework_TestCase') ||
+    version_compare(PHPUnit_Runner_Version::id(), '3.5') < 0
+) {
+    die('PHPUnit framework is required, at least 3.5 version');
+}
